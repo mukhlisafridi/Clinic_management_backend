9ad4989c9dc585a32dfb093d7f1caead34827d1d
mukhlisafridi <mukhlisafridi4@gmail.com>
2025-11-10 15:03:24 +0500

    updated

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ import mongoose from "mongoose";
   dotenv.config()
 const app = express();
 app.use(cors({
- origin: 'http://localhost:5173', 
+ origin: 'https://clinic-management-frontend-flame.vercel.app', 
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
 }));
