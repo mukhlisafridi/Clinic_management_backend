6e6ec122cd6e41cc1e9ce874fa6c8ca11e072cd5
mukhlisafridi <mukhlisafridi4@gmail.com>
2025-11-20 16:57:56 +0500

    changing

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "https://clinic-management-frontend-flame.vercel.app",
+    origin: ["https://clinic-management-frontend-flame.vercel.app","http://localhost:5173"],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
