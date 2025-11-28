233ed0c41cdd732fe6c285cac92dcb1139a774ef
mukhlisafridi <mukhlisafridi4@gmail.com>
2025-11-28 16:56:59 +0500

    updated

--- router/userRouter.js
+++ router/userRouter.js
@@ -24,14 +24,15 @@ router.post("/patient/register", patientRegister);
 router.post("/login", login);
 router.post("/admin/addnew", isAdminAuthenticated, addNewAdmin);
 router.post("/doctor/addnew", isAdminAuthenticated, addNewDoctor);
-router.delete("/doctor/delete/:id", isAdminAuthenticated, deleteDoctor);
-router.post("/doctor/addnew/simple", isAdminAuthenticated, addNewDoctorSimplified);
 router.get("/doctors", getAllDoctors);
+router.post("/doctor/addnew/simple", isAdminAuthenticated, addNewDoctorSimplified);
 router.get("/admin/me", isAdminAuthenticated, getUserDetails);
 router.get("/patient/me", isPatientAuthenticated, getUserDetails);
 router.get("/doctor/me", isDoctorAuthenticated, getUserDetails);
 router.get("/admin/logout", isAdminAuthenticated, logoutAdmin);
 router.get("/patient/logout", isPatientAuthenticated, logoutPatient);
 router.get("/doctor/logout", isDoctorAuthenticated, logoutDoctor);
+router.delete("/doctor/delete/:id", isAdminAuthenticated, deleteDoctor);
+
 
 export default router
